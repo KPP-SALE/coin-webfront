f769ee8c4a2bd7127f9eda3503cf900cf6d951cd
SeaFalcon <seafalcon323@gmail.com>
2021-05-29 10:08:10 +0900

    Initial table commit
     - setup faker module
     - basic table

--- src/App.js
+++ src/App.js
@@ -1,25 +1,84 @@
-import logo from './logo.svg';
-import './App.css';
+import useWebSocket, { ReadyState } from 'react-use-websocket';
+import React, { useState, useCallback, useMemo, useRef } from 'react';
+import faker from 'faker/locale/ko';
 
+import Table from './Table';
+
+faker.seed(100);
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const columns = ["Name", "Email", "Phone"]
+
+  const data = Array(53)
+    .fill()
+    .map(() => ({
+      name: faker.name.lastName() + faker.name.firstName(),
+      email: faker.internet.email(),
+      phone: faker.phone.phoneNumber(),
+    }))
+
+  return <Table columns={columns} data={data} />
 }
 
 export default App;
+
+// const App = () => {
+//   //Public API that will echo messages sent to it back to the client
+//   const [socketUrl, setSocketUrl] = useState('wss://echo.websocket.org');
+//   // const [socketUrl, setSocketUrl] = useState('wss://api.upbit.com/websocket/v1');
+//   const messageHistory = useRef([]);
+
+//   const {
+//     sendMessage,
+//     lastMessage,
+//     readyState,
+//   } = useWebSocket(socketUrl);
+
+//   messageHistory.current = useMemo(() =>
+//     messageHistory.current.concat(lastMessage), [lastMessage]);
+
+//   const handleClickChangeSocketUrl = useCallback(() =>
+//     // setSocketUrl('wss://demos.kaazing.com/echo'), []);
+//     setSocketUrl('wss://api.upbit.com/websocket/v1'), []);
+
+//   const handleClickSendMessage = useCallback(() =>
+//     sendMessage('[{"ticket":"test"},{"type":"ticker","codes":["KRW-BTC"]}]'), []);
+
+//   const connectionStatus = {
+//     [ReadyState.CONNECTING]: 'Connecting',
+//     [ReadyState.OPEN]: 'Open',
+//     [ReadyState.CLOSING]: 'Closing',
+//     [ReadyState.CLOSED]: 'Closed',
+//     [ReadyState.UNINSTANTIATED]: 'Uninstantiated',
+//   }[readyState];
+
+//   return (
+//     <div>
+//       <button
+//         onClick={handleClickChangeSocketUrl}
+//       >
+//         Click Me to change Socket Url
+//       </button>
+//       <button
+//         onClick={handleClickSendMessage}
+//         disabled={readyState !== ReadyState.OPEN}
+//       >
+//         Click Me to send 'Hello'
+//       </button>
+//       <span>The WebSocket is currently {connectionStatus}</span>
+//       {lastMessage ? <span>Last message: {lastMessage.data}</span> : null}
+//       <ul>
+//         {/* {messageHistory.current
+//           .map((message, idx) => <span key={idx}>{message.data}</span>)} */}
+//         sendMessage: {JSON.stringify(sendMessage)}
+//       </ul>
+//       <div>
+//         lastMessage: {JSON.stringify(lastMessage)}
+//       </div>
+//       <div>
+//         readyState: {JSON.stringify(readyState)}
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default App;
